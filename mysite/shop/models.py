4fdefb8dff27deac30a4eeb82016feb1db4ae3a7
DonatasNoreika <donoras@gmail.com>
2023-12-22 11:55:44 +0200

    +Užsakyme atvaizduoti užsakymo eilutes +Paskaičiuotų kiekvienos užsakytos prekės sumą (prekės kaina * kiekis). +Paskaičiuotų bendrą užsakymo sumą.

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -22,13 +22,23 @@ class Order(models.Model):
     date = models.DateTimeField(verbose_name="Data", auto_now_add=True)
     status = models.ForeignKey(to="Status", verbose_name="Būsena", on_delete=models.SET_NULL, null=True, blank=True)
 
+    def total(self):
+        total = 0
+        lines = self.lines.all()
+        for line in lines:
+            total += line.suma()
+        return total
+
     def __str__(self):
         return str(self.date)
 
 class OrderLine(models.Model):
-    order = models.ForeignKey(to="Order", verbose_name="Užsakymas", on_delete=models.CASCADE)
+    order = models.ForeignKey(to="Order", verbose_name="Užsakymas", on_delete=models.CASCADE, related_name="lines")
     product = models.ForeignKey(to="Product", verbose_name="Prekė", on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(verbose_name="Kiekis")
+
+    def suma(self):
+        return self.product.price * self.quantity
 
     def __str__(self):
         return f"{self.order} ({self.product} - {self.quantity})"
